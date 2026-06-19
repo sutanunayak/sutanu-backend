dfc81976a895da86460c62473e652fc8722555bc
Sutanu Nayak <sutanunayak11@gmail.com>
2026-06-19 16:38:00 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//router
+
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+//Ex- http://localhost:8000/api/v1/users/register
+
 export { app }
